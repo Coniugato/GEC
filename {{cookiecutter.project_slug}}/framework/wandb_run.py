a7ea71733be9fcc3698c3a06c7bf0a9cbc7bcc16
Ryusei Ishikawa <zgdtkm@gmail.com>
2025-11-20 19:24:16 +0900

    Fixed error about sweep

--- {{cookiecutter.project_slug}}/framework/wandb_run.py
+++ {{cookiecutter.project_slug}}/framework/wandb_run.py
@@ -1,8 +1,16 @@
 import context
+class ContextAwareRunProxy:
+    def _get_active_run(self):
+        try:
+            return context._reporter_run_context.get()
+        except LookupError:
+            raise Exception("FATAL: run (wandb run) was referenced in framework/wandb_init.py before the config was set in framework/hydraset.py. " \
+                    "Hint: Make sure never to import framework/wandb_init.py directly (or access 'run' outside of the active context).")
 
-try:
-    run = context._reporter_run_context.get()
-except LookupError:
-    raise Exception("FATAL: run (wandb run) was referenced in framework/wandb_init.py before the config was set in framework/hydraset.py. " \
-            "Hint: Make sure never to import framework/wandb_init.py directly.")
+    def __getattr__(self, name):
+        return getattr(self._get_active_run(), name)
 
+    def __setattr__(self, name, value):
+        setattr(self._get_active_run(), name, value)
+
+run = ContextAwareRunProxy()
