2f1b73c4b9a1c09821219f74cd73f9740415a8d9
Ryusei Ishikawa <zgdtkm@gmail.com>
2025-11-20 22:52:49 +0900

    config.yamlがsweepで更新されない問題を修正

--- {{cookiecutter.project_slug}}/framework/config.py
+++ {{cookiecutter.project_slug}}/framework/config.py
@@ -1,9 +1,29 @@
 import context
 from omegaconf import DictConfig
 
-try:
-    cfg: DictConfig = context._reporter_config_context.get()
-except LookupError:
-    raise Exception("FATAL: config was referenced in framework/config.py before the config was set in framework/hydraset.py. " \
-            "Hint: Make sure never to import framework/config.py directly.")
+class ContextAwareConfigProxy:
+    def _get_active_cfg(self) -> DictConfig:
+        try:
+            return context._reporter_config_context.get()
+        except LookupError:
+            raise Exception("FATAL: config was referenced in framework/config.py before the config was set in framework/hydraset.py. " \
+                    "Hint: Make sure never to import framework/config.py directly (or access 'cfg' outside of the active context).")
+
+
+    def __getattr__(self, name):
+        return getattr(self._get_active_cfg(), name)
+
+    def __getitem__(self, key):
+        return self._get_active_cfg()[key]
+
+    def __contains__(self, key):
+        return key in self._get_active_cfg()
+
+    def __repr__(self):
+        try:
+            return repr(self._get_active_cfg())
+        except:
+            return "<ContextAwareConfigProxy: No active config>"
+
+cfg = ContextAwareConfigProxy()
 
